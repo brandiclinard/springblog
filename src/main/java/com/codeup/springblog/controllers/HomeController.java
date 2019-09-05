2c774d1815f248ef3c99a6eafca6ecdb97d952cc
Brandi <brandiclinard021911@gmail.com>
2019-09-05 15:26:19 -0500

    landing page message set

---
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String landing() {
+        return "<h1>THIS IS THE LANDING PAGE!!</h1>";
+    }
+
+}
